dc86b9a41ff2ae64c6c1c6350df3348a41f6a12f
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-10-25 16:53:40 -0400

    renamed base class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class cqedu::base {
+class cqedu {
   Yumrepo['epel'] -> Package<| tag == Restic::Package |>
   exec { 'systemctl start restic_restore_restore_edx_repo':
     unless  => 'ls /backups/prod'
