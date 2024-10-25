2ffccc7f037f2682ee7e1b3ce3faf7acbb3cdaf5
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-10-25 16:59:56 -0400

    fix commas

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,8 @@
 class cqedu {
   Yumrepo['epel'] -> Package<| tag == Restic::Package |>
   exec { 'systemctl start restic_restore_restore_edx_repo':
-    unless  => 'ls /backups/prod'
-    require => Service['restic_restore_restore_edx_repo']
+    unless  => 'ls /backups/prod',
+    require => Service['restic_restore_restore_edx_repo'],
     before  => Package['tutor']
   }
 }
